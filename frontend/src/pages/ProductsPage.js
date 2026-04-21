c9e987a3d0a10579c6e6cf96767b3c5f28dc0f2b
Ritesh Bhandari <riteshbhandari678@gmail.com>
2026-04-21 07:10:48 +0545

    fix the error

--- frontend/src/pages/ProductsPage.js
+++ frontend/src/pages/ProductsPage.js
@@ -243,21 +243,19 @@ const ProductsPage = () => {
           >
             <FiFilter size={16} /> Filters
           </button>
-          <select
-            value={filters.sort}
-            onChange={(e) => updateFilter('sort', e.target.value)}
-            className="border border-gray-300 rounded-lg px-3 py-2 text-sm focus:outline-none focus:ring-2 focus:ring-amber-500"
-          >
-            {sortOptions.map(({ value, label }) => (
-              <option 
-                key={value} 
-                value={value} 
-                className="text-black" // This ensures the dropdown text is black
-              >
-                {label}
-              </option>
-            ))}
-          </select>
+         <select
+  value={filters.sort}
+  onChange={(e) => updateFilter('sort', e.target.value)}
+  /* Added text-black below to make "Newest First" visible in the box */
+  className="text-black border border-gray-300 rounded-lg px-3 py-2 text-sm focus:outline-none focus:ring-2 focus:ring-amber-500"
+>
+  {sortOptions.map(({ value, label }) => (
+    /* Added className="text-black" to make the list items black */
+    <option key={value} value={value} className="text-black">
+      {label}
+    </option>
+  ))}
+</select>
         </div>
       </div>
 
